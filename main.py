6ff21430328b0abf865de65b25b648ec5a3865ec
JValtteri <46611993+JValtteri@users.noreply.github.com>
2024-04-12 21:26:06 +0300

    Refactored program state

--- main.py
+++ main.py
@@ -5,37 +5,12 @@
 # Main program
 # 12. Mar. 2024
 
+import state as s
+import menu_main
 
-import menu_main, mailer
-
-class ProgramState():
-    '''
-    Maintains the main program state
-    '''
-
-    def __init__(self):
-        self.login    = False
-        self.settings = None
-        self.mailbox  = None
-
-    def logout(self):
-        self.mailbox.logout()
-        return "Logged out"
-
-    def mail_login(self):
-        address      = self.settings.get_address
-        pw           = "*"*len(self.settings.get_password)
-        imap         = self.settings.get_map
-        smtp         = self.settings.get_smtp
-        print(f"Addr:\t{address}\npw:\t{pw}\n")
-        print(f"IMAP:\t{imap['addr']},\t{imap['port']}")
-        print(f"SMTP:\t{smtp['addr']},\t{smtp['port']}")
-        self.mailbox   = mailer.Mailbox(self.settings)
-        status_message = self.mailbox.status_message ## TODO HERE!!!
-        return status_message
 
 def main():
-    state          = ProgramState()
+    state             = s.ProgramState()
     status_message = menu_main.menu(state)
     print(f"\n:: {status_message}")
 
---
+++ state.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# SEEC - Secure Encrypted Email Client
+# Programming project for Secure Programming course at TUNI
+#
+# Menu - Main
+# 12. Apr. 2024
+
+import mailer
+
+
+class ProgramState():
+    '''
+    Maintains the main program state
+    '''
+
+    def __init__(self):
+        self.login    = False
+        self.settings = None
+        self.mailbox  = None
+
+    def logout(self):
+        self.mailbox.logout()
+        return "Logged out"
+
+    def mail_login(self):
+        address      = self.settings.get_address
+        pw           = "*"*len(self.settings.get_password)
+        imap         = self.settings.get_map
+        smtp         = self.settings.get_smtp
+        print(f"Addr:\t{address}\npw:\t{pw}\n")
+        print(f"IMAP:\t{imap['addr']},\t{imap['port']}")
+        print(f"SMTP:\t{smtp['addr']},\t{smtp['port']}")
+        self.mailbox   = mailer.Mailbox(self.settings)
+        status_message = self.mailbox.status_message ## TODO HERE!!!
+        return status_message
